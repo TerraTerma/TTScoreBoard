287bd880b51186fe57772dc195a473852826bb59
JustMaffie Development <itisjori@gmail.com>
2016-08-17 19:28:05 +0200

    PermissionColors enum is now a class, not an enum in a class.

--- src/me/JustMaffie/TerraTerma/TTScoreBoard/Managers/NameTagManager.java
+++ src/me/JustMaffie/TerraTerma/TTScoreBoard/Managers/NameTagManager.java
@@ -1,22 +1,17 @@
 package me.JustMaffie.TerraTerma.TTScoreBoard.Managers;
 
-
-
-import java.util.Arrays;
-import java.util.Optional;
-
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 import TTCore.Entity.Living.Human.Player.TTPlayer;
+import me.JustMaffie.TerraTerma.TTScoreBoard.Util.PermissionColors;
 
 public class NameTagManager {
 	
   @SuppressWarnings("deprecation")
   public static void renewNameTag(TTPlayer player){
-      PermissionColours permission = PermissionColours.getColour(player);
+	  PermissionColors permission = PermissionColors.getColour(player);
       Player bPlayer = player.getPlayer();
           //has a permission
           Scoreboard board = bPlayer.getScoreboard();
@@ -32,59 +27,4 @@ public class NameTagManager {
           }
   }
   
-  public static enum PermissionColours {
-  	OWNER("owner", ChatColor.GOLD, ChatColor.BOLD),
-    HEAD_DEV("headdev", ChatColor.DARK_AQUA, ChatColor.BOLD),
-    DEV("developer", ChatColor.DARK_AQUA),
-    HEAD_ADMIN("headadmin", ChatColor.BLUE, ChatColor.BOLD),
-    ADMIN("Admins", ChatColor.BLUE),
-    LEAD_MOD("Lead-Mod", ChatColor.RED, ChatColor.BOLD),
-    MOD("moderator", ChatColor.RED),
-    HELPER("helper", ChatColor.AQUA),
-    BUILDER("builder", ChatColor.DARK_GREEN),
-    YOUTUBER("youtuber", ChatColor.YELLOW),
-    STARLORD("starlord", ChatColor.GOLD),
-    VIPPLUS("vip+", ChatColor.GOLD),
-    VIP("vip", ChatColor.GOLD),
-    VIPMINUS("vip-", ChatColor.GOLD),
-    DEFAULT("default", ChatColor.GRAY);	
-	  
-    
-    
-    String PERMISSION;
-    ChatColor COLOUR;
-    ChatColor SECONDARY;
-    
-    private PermissionColours(String permission, ChatColor color){
-      	PERMISSION = permission;
-      	COLOUR = color;
-    }
-    
-    private PermissionColours(String permission, ChatColor color, ChatColor second){
-      	PERMISSION = permission;
-      	COLOUR = color;
-      	SECONDARY = second;
-    }
-    
-    public String getPermission(){
-      	return PERMISSION;
-    }
-    
-    public String getColour(){
-      	if(SECONDARY == null){
-        	return COLOUR + "";
-      	}else{
-          	return COLOUR + "" + SECONDARY;
-      	}
-  	}
-    
-    public static PermissionColours getColour(TTPlayer player){
-    	String group = player.getPermissionGroup().getName();
-    	Optional<PermissionColours> opColour = Arrays.asList(values()).stream().filter(c -> c.getPermission().equals(group)).findFirst();
-    	if(opColour.isPresent()){
-    		return opColour.get();
-    	}
-    	return DEFAULT;
-    }
-  }
 }
